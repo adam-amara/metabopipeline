506a4f5f49adc9f5b4dd672b113af14e466d0cd3
maxvincent24 <maxime.vincent@philiaweb.com>
2021-03-23 16:25:12 +0100

    Remove echo from file

--- test_msconvert/dtomzML.sh
+++ test_msconvert/dtomzML.sh
@@ -10,16 +10,11 @@ VAR1=$(printf "../"'%.0s' $(eval "echo {1.."$(($length+1))"}") )
 
 for DIR in ${1}/*
 do
-	echo $DIR
-	echo $PWD
 	cd $DIR
-	echo $PWD
 	FOL=$(basename "$PWD")
-	echo $FOL
 	docker run -it --rm -e WINEDEBUG=-all -v $PWD:/data:rw test_msconvert wine msconvert *.d -o $FOL
 	mv $FOL "${VAR1}MZML"
 	cd $VAR1
-	echo $PWD
 done
 
 
